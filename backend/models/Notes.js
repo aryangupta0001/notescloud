9a87195905643beda1c7ac57b14fdcd984c73877
Aryan Gupta <aryanguptabareilly067@gmail.com>
2023-07-14 00:51:11 +0530

    created user & notes schemas

---
+++ backend/models/Notes.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const NotesSchema = new mongoose.Schema({
+    title :{
+        type: String,
+        required: true
+    },
+
+    description: {
+        type: String,
+        default: null
+    },
+
+    tags: {
+        type: String,
+        default: general
+    },
+
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+
+module.exports = mongoose.model("notes", NotesSchema);
