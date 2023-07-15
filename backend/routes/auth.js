49f5255fe33d01994c7b85b11bacdf46396a4f3a
Aryan Gupta <aryanguptabareilly067@gmail.com>
2023-07-16 01:21:28 +0530

    express-validator added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User")
 
-const { query, body } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 
 router.get("/", (req, res) => {
     obj = {
-        a: "Authentication" 
+        a: "Authentication"
     }
 
     res.json(obj)
@@ -20,13 +20,27 @@ router.get("/hello", (req, res) => {
 
 
 router.post("/", [
-    body("name").isLength({min: 5}),
-    body("email").isEmail(),
-    body("password").isLength({min: 5})
-], (req, res) => {  
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
-})
+    body("name", "Enter a valid name : ").isLength({ min: 5 }),
+    body("email", "Enter a valid email : ").isEmail(),
+    body("password", "Password should be min. 5 characters long : ").isLength({ min: 5 })
+
+    // In above validations, the second parameter is optional, it is sent to the client when any error occurs (i.e validation fails)    
+],
+    (req, res) => {
+        const result = validationResult(req);
+        if (result.isEmpty()) {
+            // return res.send(`Hello, ${req.body.name}!`);
+            User.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            }).then(user => res.send(user));
+        }
+
+        else {
+            res.send({ errors: result.array() });
+        }
+    }
+)
 
 module.exports = router
