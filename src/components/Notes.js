4a05fbe6d00509fde81450f0fba31587bd523cc2
Aryan Gupta <aryanguptabareilly067@gmail.com>
2023-08-07 16:50:31 +0530

    set Edit Modal Title

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -48,7 +48,7 @@ const Notes = () => {
                 <div className="modal-dialog" role="document">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <h5 className="modal-title" id="exampleModalLabel">Update Note</h5>
                             <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
